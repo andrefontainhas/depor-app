428ebee26b8ee770a13843136d9d0a4e25f2d1fe
André Fontainhas <fontesandre98@hotmail.com>
2025-09-10 16:32:27 +0100

    news

--- js/main.js
+++ js/main.js
@@ -74,26 +74,153 @@ document.addEventListener('DOMContentLoaded', () => {
   const jogosLista = document.querySelector('#jogos-lista');
   const jogos = [
     {
-      timeAdversario: "Adecas",
-      data: "20/04/2025",
-      hora: "16:00",
-      local: "Estádio Manuel Lima",
-      backgroundImage: "url('img/jogo-1.jpg')" // Adicione a URL da imagem de fundo
-    },
-    {
-      timeAdversario: "Futebol Clube da Vila",
-      data: "27/04/2025",
-      hora: "18:00",
-      local: "Estádio da Vila",
-      backgroundImage: "url('img/jogo-2.jpg')" // Outra imagem de fundo
-    },
-    {
-      timeAdversario: "Club de Futebol",
-      data: "03/05/2025",
-      hora: "14:30",
-      local: "Estádio Municipal de Monção",
-      backgroundImage: "url('img/jogo-3.jpg')" // Mais uma imagem
-    }
+    timeAdversario: "Vilaverdense",
+    data: "14/09/2025",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-1.jpg')"
+  },
+  {
+    timeAdversario: "CD Celoricense",
+    data: "28/09/2025",
+    hora: "15:00",
+    local: "Estádio de Celorico de Basto",
+    backgroundImage: "url('img/jogo-2.jpg')"
+  },
+  {
+    timeAdversario: "Ribeira Brava",
+    data: "05/10/2025",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-3.jpg')"
+  },
+  {
+    timeAdversario: "Mirandela",
+    data: "26/10/2025",
+    hora: "15:00",
+    local: "Estádio São Sebastião (Mirandela)",
+    backgroundImage: "url('img/jogo-4.jpg')"
+  },
+  {
+    timeAdversario: "Bragança",
+    data: "02/11/2025",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-5.jpg')"
+  },
+  {
+    timeAdversario: "Limianos",
+    data: "09/11/2025",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-6.jpg')"
+  },
+  {
+    timeAdversario: "Desp. Chaves B",
+    data: "29/11/2025",
+    hora: "15:00",
+    local: "Complexo Desportivo de Chaves",
+    backgroundImage: "url('img/jogo-7.jpg')"
+  },
+  {
+    timeAdversario: "Tirsense",
+    data: "07/12/2025",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-8.jpg')"
+  },
+  {
+    timeAdversario: "Vianense",
+    data: "14/12/2025",
+    hora: "15:00",
+    local: "Estádio Dr. José de Matos (Viana do Castelo)",
+    backgroundImage: "url('img/jogo-9.jpg')"
+  },
+  {
+    timeAdversario: "S. Martinho",
+    data: "21/12/2025",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-10.jpg')"
+  },
+  {
+    timeAdversario: "Brito",
+    data: "10/01/2026",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-11.jpg')"
+  },
+  {
+    timeAdversario: "Machico",
+    data: "18/01/2026",
+    hora: "15:00",
+    local: "Estádio Municipal de Machico (Madeira)",
+    backgroundImage: "url('img/jogo-12.jpg')"
+  },
+  {
+    timeAdversario: "Camacha",
+    data: "25/01/2026",
+    hora: "15:00",  
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-13.jpg')"
+  },
+  {
+    timeAdversario: "Vilaverdense",
+    data: "08/02/2026",
+    hora: "15:00",
+    local: "Estádio Municipal de Vila Verde",
+    backgroundImage: "url('img/jogo-14.jpg')"
+  },
+  {
+    timeAdversario: "CD Celoricense",
+    data: "15/02/2026",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-15.jpg')"
+  },
+  {
+    timeAdversario: "Ribeira Brava",
+    data: "22/02/2026",
+    hora: "15:00",
+    local: "Estádio Municipal da Ribeira Brava (Madeira)",
+    backgroundImage: "url('img/jogo-16.jpg')"
+  },
+  {
+    timeAdversario: "Mirandela",
+    data: "01/03/2026",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-17.jpg')"
+  },
+  {
+    timeAdversario: "Bragança",
+    data: "08/03/2026",
+    hora: "15:00",
+    local: "Estádio Municipal de Bragança",
+    backgroundImage: "url('img/jogo-18.jpg')"
+  },
+  {
+    timeAdversario: "Limianos",
+    data: "15/03/2026",
+    hora: "15:00",
+    local: "Estádio dos Arcos (Ponte de Lima)",
+    backgroundImage: "url('img/jogo-19.jpg')"
+  },
+  {
+    timeAdversario: "Desp. Chaves B",
+    data: "22/03/2026",
+    hora: "15:00",
+    local: "Estádio Manuel Lima (Monção)",
+    backgroundImage: "url('img/jogo-20.jpg')"
+  },
+  {
+    timeAdversario: "Tirsense",
+    data: "05/04/2026",
+    hora: "16:00",
+    local: "Estádio Abel Alves de Figueiredo (Santo Tirso)",
+    backgroundImage: "url('img/jogo-21.jpg')"
+  }
+
   ];
 
   jogos.forEach(jogo => {
